592d59add9ec4ad989db5d51a8b4b142d537c976
kellmusic <49132383+kellmusic@users.noreply.github.com>
2023-12-30 15:21:04 -0500

    got most of automatic contribution scraping, updated contribution example

--- src/main/import_investment_activity.py
+++ src/main/import_investment_activity.py
@@ -7,7 +7,13 @@ from dotenv import load_dotenv
 import os
 
 
-def grab_contributions():
+def scrape_contributions():
+    """Download contributions CSV from the year to date
+
+    :return:
+    """
+    # TODO Make it click the Contributions tab
+
     # Import secret from .env file
     load_dotenv()
     user = os.getenv("TSP_USER")
@@ -15,8 +21,10 @@ def grab_contributions():
     mfa = os.getenv("TSP_MFA")
 
     # Load TSP webpage
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option("prefs", {"download.default_directory": os.getcwd()})
+    driver = webdriver.Chrome(options=chrome_options)
     url = 'https://www.tsp.gov/login/'
-    driver = webdriver.Chrome()
     driver.get(url)
     time.sleep(3)
 
@@ -32,49 +40,74 @@ def grab_contributions():
     driver.find_element(by=By.NAME, value="answer").send_keys(mfa)
     time.sleep(1)
     driver.find_element(by=By.XPATH, value="//*[@class='button button-primary']").click()
-    time.sleep(10)
+    time.sleep(15)
 
     # Get contributions
-    try:  # TODO get it to work regardless of window size
-        driver.find_element(by=By.XPATH, value="//a[text()='Contributions ']").click()
-        time.sleep(1)
-    except:
-        driver.find_element(by=By.ID, value="mob-menu-icon']").click()
-        driver.find_element(by=By.XPATH, value="//a[text()='Contributions']").click()
-        time.sleep(1)
+    # full_xpath = '/html/body/al-app/div[2]/app-page-host/div/al-primary-page/div/div/header/div/al-layout-header-wc/div/div/div/thrive-header-widget-wc/div/div/div[2]/div[1]/div[1]/div/ul/li[5]/span/a'
+    # driver.find_element(by=By.XPATH, value=full_xpath).click()
+    # driver.find_element(by=By.XPATH, value="//a[text()='Contributions']").click()
+
+    # TODO get it to work regardless of window size
+    #  options.add_argument("window-size=1200x600")
+    # try:
+    #     driver.find_element(by=By.ID, value="primary__3272f498_6961_431d_a2cb_848499a7113esptr58e0cd48_33a7_493c_a6f6_e46d599cca72").click()
+    #     # driver.find_element(by=By.XPATH, value="//a[text()='Contributions']").click()
+    #     # primary__3272f498_6961_431d_a2cb_848499a7113esptr58e0cd48_33a7_493c_a6f6_e46d599cca72
+    #     time.sleep(1)
+    # except:
+    #     driver.find_element(by=By.ID, value="mob-menu-icon']").click()
+    #     driver.find_element(by=By.XPATH, value="//a[text()='Contributions']").click()
+    #     time.sleep(1)
 
     driver.find_element(by=By.XPATH, value="//a[text()='Account Activity']").click()
-    time.sleep(1)
+    time.sleep(2)
     driver.find_element(by=By.XPATH, value="//span[text()='Download']").click()
     time.sleep(1)
+    driver.find_element(by=By.XPATH, value="//button[@title='Download']").click()
+    time.sleep(3)
 
-
+    # Import contribution CSV
+    import_contributions(os.path.join(os.getcwd(), 'InvestmentActivityDetail.csv'))
     driver.close()
 
 
-def import_contributions():
+def import_contributions(tsp_investment_activity_path):
+    """
+    Import InvestmentActivityDetail.csv file from TSP and personal contributions.csv file and append new
+    contribution activity
+
+    :param tsp_investment_activity_path:
+    :return:
+    """
+
     contrib_file = pd.read_csv(join('resources', 'contributions.csv'))
-    tsp_ia_file = pd.read_csv(join('resources', 'InvestmentActivityDetail.csv'))
+    tsp_ia_file = pd.read_csv(tsp_investment_activity_path)
     ia_df = pd.DataFrame(columns=contrib_file.columns)
     for date, grp in tsp_ia_file.groupby('VALUATION DATE'):
-        ia_df = ia_df.append({'Date': grp.values[0][0].replace('-', '/'),
-                              'Traditional': grp[grp['ACTIVITY TYPE'] == 'Automatic (1%)']['AMOUNT'].sum(),
-                              'Roth': grp[grp['ACTIVITY TYPE'] == 'Roth']['AMOUNT'].sum(),
-                              'Automatic_1': grp[grp['ACTIVITY TYPE'] == 'Automatic (1%)']['AMOUNT'].sum(),
-                              'Matching': grp[grp['ACTIVITY TYPE'] == 'Match']['AMOUNT'].sum(),
-                              'Total': grp['AMOUNT'].sum(),
-                              'C FUND': grp[grp['FUND'] == 'C Fund']['FUND UNITS'].sum(),
-                              'S FUND': grp[grp['FUND'] == 'S Fund']['FUND UNITS'].sum(),
-                              }, ignore_index=True)
+        ia_row = {'Date': grp.values[0][0].replace('-', '/'),
+                  'Traditional': grp[grp['ACTIVITY TYPE'] == 'Traditional']['AMOUNT'].sum(),
+                  'Roth': grp[grp['ACTIVITY TYPE'] == 'Roth']['AMOUNT'].sum(),
+                  'Automatic_1': grp[grp['ACTIVITY TYPE'] == 'Automatic (1%)']['AMOUNT'].sum(),
+                  'Matching': grp[grp['ACTIVITY TYPE'] == 'Match']['AMOUNT'].sum(),
+                  'Total': grp['AMOUNT'].sum()}
+        for fund in contrib_file.columns[6:]:
+            ia_row[fund] = grp[grp['FUND'] == fund]['FUND UNITS'].sum()
+        ia_df = ia_df.append(ia_row, ignore_index=True)
         pass
     ia_df = ia_df.fillna(0)
 
     contrib_file2 = contrib_file.append(ia_df, ignore_index=True)
+    contrib_file2.to_csv(join('resources', 'contributions2.csv'), index=False)
     pass
 
 
 def scrape_tsp_performance():
-    current_prices = pd.read_csv('resources\\Share_Prices.csv')
+    """
+    Scrape public TSP fund performance and append to personal Share_Prices.csv file
+
+    :return:
+    """
+    current_prices = pd.read_csv(os.path.join('resources', 'Share_Prices.csv'))
 
     tsp_url = 'https://www.tsp.gov/share-price-history/'
     driver = webdriver.Chrome()
@@ -103,6 +136,8 @@ def scrape_tsp_performance():
 
 
 if __name__ == '__main__':
+    import_contributions(os.path.join(os.getcwd(), 'InvestmentActivityDetail.csv'))
+
     # import_contributions()
     # scrape_tsp_performance()
-    grab_contributions()
+    # grab_contributions()
